f4878655bedb468591514da70e311e94307f1c71
zenjutahi <zenjutahi@gmail.com>
2020-03-16 12:13:35 +0300

    [feature] - add card-list component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { CardList } from './componets/card-list/card-list.componet';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      animes : []
+    };
+  }
+
+  UNSAFE_componentWillMount() {
+    fetch('http://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(wasee => this.setState({ animes: wasee}))
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <CardList>
+          {this.state.animes.map(character => (
+            <h1 key={character.id}> {character.name} </h1>
+            ))}
+        </CardList>
+      </div>
+    );
+  }
 }
 
 export default App;
